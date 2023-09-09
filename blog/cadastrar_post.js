bc2ecd5bd6c7af86ff55f5a68431f908c7ac4acb
drmcarvalho <furiano.k@gmail.com>
2023-09-09 01:24:48 -0300

    const

--- blog/cadastrar_post.js
+++ blog/cadastrar_post.js
@@ -2,14 +2,13 @@ const qs = require('querystring');
 const database = require('./database');
 
 module.exports = function(request, response) {
-    let body = '';
-    let obj = null;
+    let body = '';    
     request
         .on('data', chunk => {
             body += chunk;    
         })
         .on('end', () => {
-            obj = qs.parse(body);            
+            const obj = qs.parse(body);
             if ('titulo' in obj && 'conteudo' in obj)
                 database.cadastrar_post(obj.titulo, obj.conteudo);
             else
