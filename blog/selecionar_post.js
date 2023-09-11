0101c2e2e4818c468684840b322c458e5b1b2665
drmcarvalho <furiano.k@gmail.com>
2023-09-11 10:29:33 -0300

    package e pagina de post

--- blog/selecionar_post.js
+++ blog/selecionar_post.js
@@ -1,7 +1,16 @@
 const database = require('./database.js');
 const template = require('./template.js');
 
-module.exports = function(request, response, params) {    
-    response.write("Id informado " + params.id);    
-    response.end();
+module.exports = function(request, response, params) {        
+    database.query(function(result) {        
+        if (result && result.length) {
+            titulo = result[0].titulo;
+            conteudo = result[0].conteudo;
+            response.write(template.base_html_post.replace('@titulo', titulo).replace('@conteudo', conteudo));
+        }                
+        else {
+            response.write(template.base_html_post.replace('@titulo', 'Post não encontrado').replace('@conteudo', ''));
+        }
+        response.end();                        
+    },"select titulo, conteudo from posts where id = ?", [params.id]);
 }
