fa9b9e4e7f70821390a5408ae842c0332e31a2c3
drmcarvalho <furiano.k@gmail.com>
2023-09-11 15:44:30 -0300

    fix template

--- blog/listar_posts.js
+++ blog/listar_posts.js
@@ -11,14 +11,14 @@ module.exports = function(request, response, status) {
                         <h2>${post.titulo}</h2> 
                         <p>${post.conteudo.slice(3, 20) + '...'}</p> 
                         <footer> 
-                            Publicado em <time datetime="${post.publicado_em}">${post.publicado_em}</time> 
+                            <p style="font-size: 1.3vh;">Publicado em <time datetime="${post.publicado_em}">${post.publicado_em}</time></p>
                         </footer> 
                     </a>
                 </article>
             `);
         }
         
-        let template_final_posts = template.base_lista_post_html.replace('@postagens', postagens_template_html.join(""));
+        const template_final_posts = template.base_lista_post_html.replace('@postagens', postagens_template_html.join(""));
         response.write(template.base_html_cyberia.replace('@template', template_final_posts).replace('@identificador', ''));
         response.end();
     },"select * from posts", []);
