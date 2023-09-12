bfe73840492791e977f064c1a159486ac0589bc0
drmcarvalho <furiano.k@gmail.com>
2023-09-11 23:10:22 -0300

    route pattern index

--- routes.js
+++ routes.js
@@ -12,8 +12,10 @@ module.exports = (app) => {
         return `Error page: ${status}`;
     });
 
+    app.setPattern('index', 'listar_posts|index|home|inicio');
 
-    app.action('GET', '/listar_posts', './blog/listar_posts.js');
+    app.action('GET', '/', './blog/listar_posts.js');
+    app.action('GET', '/<myindex:index>', './blog/listar_posts.js');
     app.action('POST', '/cadastrar_post/<chaveacesso>', './blog/cadastrar_post.js');
     app.action('GET', '/post/<id:num>', './blog/selecionar_post.js');
     app.action('POST', '/deletar_post/<chaveacesso>-<id:num>', './blog/deletar_post.js');
